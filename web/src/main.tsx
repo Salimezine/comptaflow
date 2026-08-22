13eb5dfa97c123e8b60c8a518557d42aa2af6b91
med salim ezzine <ezzinesalim21@gmail.com>
2026-08-22 13:04:41 +0100

    fix: BrowserRouter basename /comptaflow for GitHub Pages SPA routing

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/comptaflow">
       <App />
     </BrowserRouter>
   </React.StrictMode>
